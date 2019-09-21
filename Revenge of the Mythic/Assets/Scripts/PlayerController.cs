63f2390cceeaad0fdff95bec946d8a9d9e3fd799
Josh <jbromber@my.centennialcollege.ca>
2019-09-21 16:29:37 -0400

    FInished adding Player Axises. Finished Player Controller
    Instances of Player Controller remain incomplete.

--- Revenge of the Mythic/Assets/Scripts/PlayerController.cs
+++ Revenge of the Mythic/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Currently worked on by: Josh Bromberg
-//Please do not touch without informing whoever is working on this file
+
+
 public abstract class PlayerController : MonoBehaviour
 {
     #region Hidden Variables
@@ -12,6 +12,7 @@ public abstract class PlayerController : MonoBehaviour
     private int[] abilityCooldownReset;
     private CapsuleCollider2D basicAttackRange;
     private BoxCollider2D playerCollider;
+    private string[] attackAxis;
     #endregion
 
     //Private Variables
@@ -32,6 +33,7 @@ public abstract class PlayerController : MonoBehaviour
     protected Rigidbody2D rBody { get { return rbody; } }
     protected int[] AbilityCooldownReset { get { return abilityCooldownReset; } }
     protected CapsuleCollider2D BasicAttackRange { get { return basicAttackRange; } }
+    protected string[] AttackAxis { get { return attackAxis; } }
     #endregion
     #region Public Properties
     public BoxCollider2D PlayerCollider { get { return playerCollider; } }
@@ -40,12 +42,16 @@ public abstract class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        #region Set X/Y Axis
-        instance = GameObject.FindGameObjectsWithTag("Player").Length - 1;
+        #region Set Player-Based Axises
+        instance = GameObject.FindGameObjectsWithTag("Player").Length;
         axisX = "Horizontal" + instance;
         axisY = "Vertical" + instance;
+        attackAxis = new string[4];
+        attackAxis[0] = "P" + instance + "BasicAttack";
+        for (int z = 1; z < 4; ++z) {
+            attackAxis[z] = "P" + instance + "Ability" + z;
+        }
         #endregion
-        //TODO: Make other Axises work
         rbody = GetComponent<Rigidbody2D>();
 
     }
@@ -57,6 +63,16 @@ public abstract class PlayerController : MonoBehaviour
         float horiz = Input.GetAxis(axisX);
         float vert = Input.GetAxis(axisY);
         rBody.velocity *= new Vector2(horiz, vert);
+        #endregion
+        #region Attack
+        if (globalCooldown == 0)
+        {
+            for (int z = 0; z < attackAxis.Length; ++z) {
+                if (Input.GetAxis(attackAxis[z]) > 0) {
+                    attack(z);
+                }
+            }
+        }
         #endregion
     }
 
@@ -102,7 +118,7 @@ public abstract class PlayerController : MonoBehaviour
 /*
 Player Controls:
 WASD - Movement (done)
-ZXCV - Attacks (incomplete)
+ZXCV - Attacks (done)
 Collisions - Not to be included unless necessary
     Enemy Collisions - Each enemy should have their own collision, in case they don't outright kill the player
     Item Collisions - Each item should be responsible for the benifits it provides
