c7d1069deeedc7506f8e54f31ce26fd8bd6e5c14
Josh <jbromber@my.centennialcollege.ca>
2019-09-21 14:53:10 -0400

    Added Player Controller
    Tis not done

---
+++ Revenge of the Mythic/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Currently worked on by: Josh Bromberg
+//Please do not touch without informing whoever is working on this file
+public abstract class PlayerController : MonoBehaviour
+{
+    #region Hidden Variables
+    private int instance;
+    private string axisX, axisY;
+    private Rigidbody2D rbody;
+    private int[] abilityCooldownReset;
+    #endregion
+
+    //Private Variables
+    private int speed = 10;
+
+    #region Cooldowns
+    private readonly int globalCooldownReset = 25;
+    private int globalCooldown = 0;
+    private int[] abilityCooldown = new int[3];
+    #endregion
+
+    //Properties
+    protected int Instance { get { return instance; } }
+    protected string AxisX { get { return axisX; } }
+    protected string AxisY { get { return axisY; } }
+    protected Rigidbody2D rBody { get { return rbody; } }
+    protected int[] AbilityCooldownReset { get { return abilityCooldownReset; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region Set X/Y Axis
+        instance = GameObject.FindGameObjectsWithTag("Player").Length - 1;
+        axisX = "Horizontal" + instance;
+        axisY = "Vertical" + instance;
+        #endregion
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Move Player
+        float horiz = Input.GetAxis(axisX);
+        float vert = Input.GetAxis(axisY);
+        rBody.Velocity *= new Vector2(horiz, vert);
+        #endregion
+    }
+
+    #region Attacks
+    private void attack(int i)
+    {
+        globalCooldown = globalCooldownReset;
+        switch (i)
+        {
+            case 0:
+                BasicAttack();
+                break;
+            case 1:
+                abilityCooldown[0] = abilityCooldownReset[0];
+                Attack1();
+                break;
+            case 2:
+                abilityCooldown[1] = abilityCooldownReset[1];
+                Attack2();
+                break;
+            case 3:
+                abilityCooldown[2] = abilityCooldownReset[2];
+                Attack3();
+                break;
+        }
+    }
+    private void BasicAttack()
+    {
+        foreach(GameObject g in /*[Game Objects in Area]*/){
+            try
+            {
+                g.damage(2);
+            }
+            catch (/*MethodNotFoundException e*/) { };
+        }
+    }
+    protected abstract void Attack1();
+    protected abstract void Attack2();
+    protected abstract void Attack3();
+    #endregion
+}
