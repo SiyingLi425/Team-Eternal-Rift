9cce38a4ceee95cea8741eb6809bb14196b38415
SIying Li <yukimashiro425@gmail.com>
2019-10-02 10:10:17 -0400

    Started Tutorial Script
    also added enemy damage method which I forgot about yesterday.

---
+++ Revenge of the Mythic/Assets/Scripts/TurorialController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurorialController : MonoBehaviour
+{
+    public Text speakerName;
+    public Text dialogueText;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
