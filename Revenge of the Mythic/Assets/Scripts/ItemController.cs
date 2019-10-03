a23de3f0c5053ad743425ff15ce6700b172c1be1
SIying Li <yukimashiro425@gmail.com>
2019-10-03 00:08:57 -0400

    Completed Dialogue System and started items scripts
    Did a dialogue system so we can use it for tutorial bird plus future dialogues. Also started on items scripts

---
+++ Revenge of the Mythic/Assets/Scripts/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemController : MonoBehaviour
+{
+    public Collider2D collider, playerCollider;
+    public GameObject player;
+   
+    // Start is called before the first frame update
+    protected virtual void  Start()
+    {
+        collider = GetComponent<Collider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (playerCollider.IsTouching(collider))
+           {
+
+            effect();
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected abstract void effect();
+}
--- Revenge of the Mythic/Assets/Scripts/TurorialController.cs
+++ Revenge of the Mythic/Assets/Scripts/TurorialController.cs
@@ -5,20 +5,73 @@ using UnityEngine.UI;
 
 public class TurorialController : MonoBehaviour
 {
-    public Text speakerName;
-    public Text dialogueText;
+    public Text dialogueTextHolder;
+    public GameObject npcBox;
+    public Text nameHolder;
 
-    private Queue<string> sentences;
+    [TextArea(3, 10)]
+    public string[] sentences;
+    public string speakerName;
+
+    public Queue<string> sentencesQueve;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        sentencesQueve = new Queue<string>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void interact()
+    {
+
+        StartDialogue();
+        Debug.Log("Interact");
+        
+    }
+    public void StartDialogue()
+    {
+        Debug.Log("Start");
+        npcBox.SetActive(true);
+        nameHolder.text = speakerName;
+        sentencesQueve.Clear();
+        foreach(string sentence in sentences)
+        {
+            sentencesQueve.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void  DisplayNextSentence()
+    {
+        if(sentencesQueve.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentencesQueve.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueTextHolder.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueTextHolder.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        npcBox.SetActive(false);
     }
 }
