457f2aa65aaf801f49cc97daa4581393269117ac
Josh <jbromber@my.centennialcollege.ca>
2019-10-05 14:41:19 -0400

    Simplified ItemController. Added Gas Script
    Gaaaaaaaaaaaaaaaaas

---
+++ Revenge of the Mythic/Assets/Scripts/Gas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gas : ItemController
+{
+    protected override void effect(PlayerController p)
+    {
+        p.Damage(0, "Slow");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
