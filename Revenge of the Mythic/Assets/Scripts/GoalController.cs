f4258fc635777a646804e8bdd8ebedc597981edf
SIying Li <yukimashiro425@gmail.com>
2019-10-19 20:57:59 -0400

    Fixed Moving to different Scenes

--- Revenge of the Mythic/Assets/Scripts/GoalController.cs
+++ Revenge of the Mythic/Assets/Scripts/GoalController.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 public class GoalController : MonoBehaviour
 {
     private Collider2D goalCollider, playerCollider;
-    public static int level;
-    
+    public static int level = 1;
+    int timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         goalCollider = this.GetComponent<BoxCollider2D>();
         playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if (playerCollider.IsTouching(goalCollider))
+        
+        //if (playerCollider.IsTouching(goalCollider) && timer == 0)
+        //{
+        //    Debug.Log("Level: " + level);
+        //    if(level < 3)
+        //    {
+        //        level++;
+        //        UnityEngine.SceneManagement.SceneManager.LoadScene("Level" + (level + 1));
+        //    }else if(level >= 3)
+        //    {
+        //        UnityEngine.SceneManagement.SceneManager.LoadScene("Victory");
+        //    }
+        //    timer = 200;
+
+        //}
+        //timer--;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Player" &&(playerCollider.IsTouching(goalCollider)))
         {
-            if(level < 3)
+            Debug.Log("Level: " + level);
+            if (level < 3)
             {
-                level++;
                 UnityEngine.SceneManagement.SceneManager.LoadScene("Level" + (level + 1));
-            }else if(level >= 3)
+                level++;
+            }
+            else if (level >= 4)
             {
                 UnityEngine.SceneManagement.SceneManager.LoadScene("Victory");
             }
-
         }
     }
+
+
 }
