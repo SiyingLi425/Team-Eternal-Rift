8f13f961410916f4c9a3a9d9ec7042be7705bbaa
Josh <jbromber@my.centennialcollege.ca>
2019-11-02 15:49:10 -0400

    Added Pegasus
    Now to fix some rotation bugs for the new player objects...

---
+++ Revenge of the Mythic/Assets/Scripts/Pegasus.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pegasus : PlayerController
+{
+    [SerializeField]
+    private PolygonCollider2D wingBuffetRange, dashPath;
+    [SerializeField]
+    private Transform dashLocation;
+    protected override void Attack1()
+    {
+        //Ability 1 [Kick] - Melee damage. Turns around and kicks. Interrupts enemy attack. 6 seconds cool down.
+        foreach (string s in Damagable)
+        {
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag(s))
+            {
+                if (g.PrimaryCollider().enabled && BasicAttackRange.IsTouching(g.PrimaryCollider()))
+                {
+                    g.PrimaryController().Damage(3, "Knockback");
+                }
+            }
+        }
+    }
+    protected override void Attack2()
+    {
+        //Ability 2 [Dash] - A short dash that damages everything in your path. 8 seconds cooldown.
+        #region Wall Check
+        bool stop = false;
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Wall"))
+        {
+            if (dashPath.IsTouching(g.PrimaryCollider()) || g.PrimaryCollider().bounds.Contains(dashLocation.position))
+            {
+                stop = true;
+                break;
+            }
+        }
+        #endregion
+        #region Dash
+        if (stop == false)
+        {
+            foreach (string s in Damagable)
+            {
+                foreach (GameObject g in GameObject.FindGameObjectsWithTag(s))
+                {
+                    if (g.PrimaryCollider().enabled && dashPath.IsTouching(g.PrimaryCollider()))
+                    {
+                        g.PrimaryController().Damage(3);
+                    }
+                }
+            }
+            rBody.position = dashLocation.position;
+        }
+        #endregion
+    }
+    protected override void Attack3()
+    {
+        //Ability 3 [Wing Buffet] - Ranged damage cone. Knocks back. Does more damage versus bosses(instead of using knock back). 10 second cooldown.
+        foreach (string s in Damagable)
+        {
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag(s))
+            {
+                if (g.PrimaryCollider().enabled && wingBuffetRange.IsTouching(g.PrimaryCollider()))
+                {
+                    int i = g.GetComponent<Level1BossController>() ? 8 : 5;
+                    g.PrimaryController().Damage(i, "Knockback");
+                }
+            }
+        }
+    }
+}
